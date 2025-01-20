1655ac6a0ea08e33f73c879bfa882f5d0eabc3a4
Deepesh Patel <deepeshpatel28@icloud.com>
2025-01-20 00:08:34 +0000

    add excerpt and update function to models.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-deepeshpate-ithinkthere-ymx80caa1a7.ws.codeinstitute-ide.net'
-                ,'.herokuapp.com'
+ALLOWED_HOSTS = ['8000-deepeshpate-ithinkthere-ymx80caa1a7.ws.codeinstitute-ide.net',
+                '.herokuapp.com'
 ]
 
 
