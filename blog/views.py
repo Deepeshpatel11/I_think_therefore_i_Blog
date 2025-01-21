71be472cf3dfc33ef2016c03996e0f39f328fe0f
Deepesh Patel <deepeshpatel28@icloud.com>
2025-01-21 21:33:43 +0000

    add base html template static file for css: wire up these pages to settings.py

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
+    model = Post
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
